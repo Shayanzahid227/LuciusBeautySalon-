dc60d6fff8ee22b40b9824e29d5c3f4c9fada750
Shayanzahid227 <sshayanzahid227@gmail.com>
2024-12-16 15:12:04 +0500

    pin code ui compleat

--- lib/code_with_asiftaj/packeges/mixture.dart
+++ lib/code_with_asiftaj/packeges/mixture.dart
@@ -1,8 +1,27 @@
+import 'package:figma_creation_task/ui/screens/test/qqqq.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
-class ImportantPackeges extends StatelessWidget {
+class ImportantPackeges extends StatefulWidget {
   const ImportantPackeges({super.key});
+
+  @override
+  State<ImportantPackeges> createState() => _ImportantPackegesState();
+}
+
+class _ImportantPackegesState extends State<ImportantPackeges> {
+  bool isSelected = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void onClick() {
+    isSelected = !isSelected;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +54,7 @@ class ImportantPackeges extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 80.0),
                   child: PinCodeTextField(
+                    keyboardType: TextInputType.number,
                     cursorColor: Colors.orange,
                     showCursor: true,
                     appContext: context,
@@ -65,14 +85,42 @@ class ImportantPackeges extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text("didn't recive code? "),
-                    Text(
-                      "RESEND",
-                      style: TextStyle(
-                          color: Colors.green,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        "RESEND",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600),
+                      ),
                     )
                   ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    onClick();
+                  },
+                  child: Container(
+                    height: 60.h,
+                    width: 300,
+                    child: Center(
+                      child: Text(
+                        "Verify",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: isSelected ? Colors.white : Colors.black),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: isSelected
+                            ? Colors.purple[200]
+                            : Colors.green[300]),
+                  ),
                 )
               ],
             ),
